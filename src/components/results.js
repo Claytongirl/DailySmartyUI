25fc79c64ce75da89b791d129106d5dd207ac22a
Claytongirl <lisadeclayton@gmail.com>
2019-10-31 23:40:29 -0600

    put together fetch posts with query action to receive query posts

--- src/components/results.js
+++ src/components/results.js
@@ -3,10 +3,13 @@ import React, { Compnonent } from 'react';
 import Logo from './logo';
 import SearchBar from './searchBar';
 
+import { connect } from 'react-redux';
+import * as actions from"../actions";
+
 class Results extends Component {
 
     handleSearchBarSubmit(query) {
-        console.log(query);
+        this.props.fetchPostsWithQuery(query);
     }
 
     render() {
@@ -18,3 +21,4 @@ class Results extends Component {
         )
     }
 }
+export default connect(null, actions)(Results);
