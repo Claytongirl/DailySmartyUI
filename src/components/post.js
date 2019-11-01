92f64cc7ee5e395d2446b8cfaceb911d27feb887
Claytongirl <lisadeclayton@gmail.com>
2019-10-31 18:41:51 -0600

    modified logo component to take in a custom size via props

--- src/components/post.js
+++ src/components/post.js
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 class Post extends Component {
     renderTopics() {
         let topics = this.props.associated_topics.map((topic, index) => {
-            return <span className="post-topic" key={index}>{topic}></span>
+            return <span className="post-topic" key={index}>{topic}</span>
         })
         return topics;
     }
 
     render() {
         return (
-            <li className="recent-post">
+             <li className="recent-post">
                 <div className="recent-post__title">
                     {this.props.title}
                  </div>
